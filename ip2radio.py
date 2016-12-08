809360869bf42c5b63c932a3e9f8d195d673b8e0
danrl <mail@danrl.com>
2016-12-08 19:11:40 +0100

    slow down restart

--- ip2radio.py
+++ ip2radio.py
@@ -1,7 +1,15 @@
 import subprocess
 from time import sleep
 import circuit as c
+import signal
 
+def clean_exit():
+    print "clean exit"
+    c.cleanup()
+    exit(0)
+
+signal.signal(signal.SIGINT, clean_exit)
+signal.signal(signal.SIGTERM, clean_exit)
 
 stations = [
     'http://stream2.friskyradio.com:8000/frisky_mp3_hi',
@@ -14,6 +22,7 @@ def play(station):
     global stations
     global proc
     c.led_off("all")
+    c.led_on("orange")
     if proc:
         proc.terminate()
         sleep(3)
@@ -25,11 +34,11 @@ def play(station):
     ])
     # wait for stream to cache
     for _ in range(5*2):
-        c.led_on("orange")
-        sleep(0.25)
         c.led_off("orange")
         sleep(0.25)
-    c.led_on("orange")
+        c.led_on("orange")
+        sleep(0.25)
+    c.led_off("orange")
 
 play(0)
 c.led_on("green")
